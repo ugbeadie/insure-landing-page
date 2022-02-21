bb2de20d9d657a8641b7c3a299f2629def4ac0a6
ugbeadie <ugbeadie3@gmail.com>
2022-02-21 15:51:38 +0100

    insure

---
+++ script.js
@@ -0,0 +1,32 @@
+const burger = document.getElementsByClassName('burger')[0];
+const navUl = document.getElementsByClassName('nav-items')[0];
+// const navBar = document.querySelector('nav');
+// const grayBlue = 'hsl(216, 30%, 68%)';
+
+burger.addEventListener('click', () => {
+    navUl.classList.toggle('active');
+    document.body.classList.toggle('hidden')
+    // navBar.classList.toggle('nav-active')
+    // burger.classList.toggle('close')
+
+}) 
+
+// burger.addEventListener('click', () => {
+//     if (navBar.style.backgroundColor = 'grayBlue') {
+//         navBar.classList.add('nav-active');
+//     }else {
+//         navBar.classList.remove('nav-active');
+//     }
+// })
+
+window.addEventListener ('scroll', function() {
+    let navBar = document.querySelector('nav');
+    let windowPosition =  window.scrollY > 6000;
+    navBar.classList.toggle('scrolling-active', windowPosition);
+})
+
+function hideUl() {
+    document.body.classList.toggle('close');
+    document.body.classList.toggle('hidden');
+    navUl.classList.toggle('active');
+}
