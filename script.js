2a5319efa2a45abb7041e0c3f3e4167df253b686
ugbeadie <ugbeadie3@gmail.com>
2022-03-08 20:53:42 +0100

    insure

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const burger = document.getElementsByClassName('burger')[0];
+const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const navUl = document.getElementsByClassName('nav-items')[0];
 // const navBar = document.querySelector('nav');
 // const grayBlue = 'hsl(216, 30%, 68%)';
 
-burger.addEventListener('click', () => {
+toggleButton.addEventListener('click', () => {
     navUl.classList.toggle('active');
     document.body.classList.toggle('hidden')
+    document.body.classList.toggle("cross");
+
     // navBar.classList.toggle('nav-active')
     // burger.classList.toggle('close')
 
@@ -21,12 +23,12 @@ burger.addEventListener('click', () => {
 
 window.addEventListener ('scroll', function() {
     let navBar = document.querySelector('nav');
-    let windowPosition =  window.scrollY > 500;
+    let windowPosition =  window.scrollY > 600;
     navBar.classList.toggle('scrolling-active', windowPosition);
 })
 
 function hideUl() {
-    document.body.classList.toggle('close');
+    document.body.classList.toggle('cross');
     document.body.classList.toggle('hidden');
     navUl.classList.toggle('active');
 }
